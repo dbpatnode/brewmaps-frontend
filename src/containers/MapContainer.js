1361551ac0c486733cfe656a1217a7939ce09221
dbpatnodee <dbpatnode@hotmail.com>
2020-10-10 21:46:06 -0700

    brewery pins rendered

--- src/containers/MapContainer.js
+++ src/containers/MapContainer.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import Map from '../components/Map.js'
-import NavBar from '../components/NavBar.js'
+import axios from 'axios'
+
 
 class MapContainer extends React.Component {
 
+    state = {
+        pins: []
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:3000/breweries',{withCredentials: true})
+        // .then(resp => console.log(resp))
+        .then(resp => {this.setState({pins: resp.data})
+        })
+    }
+
+    
+
     render() {
+        // console.log(this.state.pins)
         return (
-            // <NavBar/>
-            <Map/>
+            <Map pins= {this.state.pins}/>
         )
     }
 }
