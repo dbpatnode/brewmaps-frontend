1e4f486b087a11a5e7ba059c041dcc9ddf1a3f4a
dbpatnodee <dbpatnode@hotmail.com>
2020-10-20 15:34:26 -0700

    changed toggle function on favorites

--- src/containers/FavoriteContainer.js
+++ src/containers/FavoriteContainer.js
@@ -75,17 +75,13 @@ class FavoriteContainer extends Component {
         width="1em"
         height="1em"
         viewBox="0 0 16 16"
-        className="bi bi-chat-left-text"
+        class="bi bi-pencil"
         fill="currentColor"
         xmlns="http://www.w3.org/2000/svg"
       >
         <path
-          fillRule="evenodd"
-          d="M14 1H2a1 1 0 0 0-1 1v11.586l2-2A2 2 0 0 1 4.414 11H14a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12.793a.5.5 0 0 0 .854.353l2.853-2.853A1 1 0 0 1 4.414 12H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"
-        />
-        <path
-          fillRule="evenodd"
-          d="M3 3.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 6a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 6zm0 2.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z"
+          fill-rule="evenodd"
+          d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5L13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175l-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"
         />
       </svg>
     );
@@ -181,7 +177,11 @@ class FavoriteContainer extends Component {
                     user={this.props.user.id}
                   ></Note>
                 )}
-                <button onClick={this.toggle}>{notes}</button>
+                <div className="note-toggle">
+                  <button onClick={this.toggle} id="note-button">
+                    {notes}
+                  </button>
+                </div>
 
                 <button
                   className="submit"
