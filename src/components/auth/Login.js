e5a5b5e02a121e823076d7e3a63025f0d5529927
Daniel Patnode <dbpatnode@gmail.com>
2022-04-20 20:40:09 -0700

    all svgs coming from svg file

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,11 +1,12 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { arrow, userIcon, password } from '../../SVGs/svg';
 
 export default class Login extends Component {
   state = {
-    username: "",
-    password: "",
-    loginErrors: "",
+    username: '',
+    password: '',
+    loginErrors: '',
     allFavorites: [],
   };
 
@@ -19,11 +20,11 @@ export default class Login extends Component {
   handleSubmit = (e) => {
     e.preventDefault();
     const { username, password } = this.state;
-    this.props.history.push("/map");
+    this.props.history.push('/map');
 
-    fetch("https://daniels-brewmaps-api.herokuapp.com/login", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
+    fetch('https://daniels-brewmaps-api.herokuapp.com/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
         user: {
           username,
@@ -34,102 +35,53 @@ export default class Login extends Component {
       .then((res) => res.json())
       .then((json) => {
         this.props.handleLogin(json.user);
-        localStorage.setItem("authToken", json.auth_token);
+        localStorage.setItem('authToken', json.auth_token);
       });
   };
 
   render() {
-    const arrow = (
-      <svg
-        width="1em"
-        height="1em"
-        viewBox="0 0 16 16"
-        class="bi bi-arrow-right"
-        fill="currentColor"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          fill-rule="evenodd"
-          d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
-        />
-      </svg>
-    );
-
-    const userIcon = (
-      <svg
-        width="1em"
-        height="1em"
-        viewBox="0 0 16 16"
-        class="bi bi-person-circle"
-        fill="currentColor"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path d="M13.468 12.37C12.758 11.226 11.195 10 8 10s-4.757 1.225-5.468 2.37A6.987 6.987 0 0 0 8 15a6.987 6.987 0 0 0 5.468-2.63z" />
-        <path fill-rule="evenodd" d="M8 9a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
-        <path
-          fill-rule="evenodd"
-          d="M8 1a7 7 0 1 0 0 14A7 7 0 0 0 8 1zM0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8z"
-        />
-      </svg>
-    );
-
-    const password = (
-      <svg
-        width="1em"
-        height="1em"
-        viewBox="0 0 16 16"
-        class="bi bi-lock"
-        fill="currentColor"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          fill-rule="evenodd"
-          d="M11.5 8h-7a1 1 0 0 0-1 1v5a1 1 0 0 0 1 1h7a1 1 0 0 0 1-1V9a1 1 0 0 0-1-1zm-7-1a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h7a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2h-7zm0-3a3.5 3.5 0 1 1 7 0v3h-1V4a2.5 2.5 0 0 0-5 0v3h-1V4z"
-        />
-      </svg>
-    );
     return (
       <div>
-        <form className="login" onSubmit={this.handleSubmit}>
+        <form className='login' onSubmit={this.handleSubmit}>
           <fieldset>
-            <legend className="legend">Login</legend>
-            <div className="input">
+            <legend className='legend'>Login</legend>
+            <div className='input'>
               <input
-                type="username"
-                name="username"
-                placeholder="Username"
+                type='username'
+                name='username'
+                placeholder='Username'
                 value={this.state.username}
                 onChange={this.handleChange}
                 required
               />
               <span>
-                <i className="fa fa-envelope-o">{userIcon}</i>
+                <i className='fa fa-envelope-o'>{userIcon}</i>
               </span>
             </div>
-            <div className="input">
+            <div className='input'>
               <input
-                type="password"
-                name="password"
-                placeholder="Password"
+                type='password'
+                name='password'
+                placeholder='Password'
                 value={this.state.password}
                 onChange={this.handleChange}
                 required
               />
               <span>
-                <i className="fa fa-lock">{password}</i>
+                <i className='fa fa-lock'>{password}</i>
               </span>
             </div>
 
-            <button className="submit" type="submit">
+            <button className='submit' type='submit'>
               {arrow}
             </button>
             {this.state.isInvalid && <div>Invalid Username or Password</div>}
           </fieldset>
         </form>
         <br />
-        <div className="route-links">
-          <Link to="/">homepage</Link> {"  🍻  "}
-          <Link to="/signup">signup </Link>
+        <div className='route-links'>
+          <Link to='/'>homepage</Link> {'  🍻  '}
+          <Link to='/signup'>signup </Link>
         </div>
       </div>
     );
--- src/containers/NoteContainer.js
+++ src/containers/NoteContainer.js
@@ -1,11 +1,12 @@
-import React, { Component } from "react";
-import ReactStars from "react-rating-stars-component";
+import React, { Component } from 'react';
+import ReactStars from 'react-rating-stars-component';
+import { pencil } from '../SVGs/svg';
 
 class NoteContainer extends Component {
   state = {
-    review: "",
-    atmosphere: "",
-    rating: "",
+    review: '',
+    atmosphere: '',
+    rating: '',
     on: false,
   };
 
@@ -29,7 +30,7 @@ class NoteContainer extends Component {
     e.preventDefault();
     const note = {
       review: this.state.review,
-      atmosphere: this.state.atmospher,
+      atmosphere: this.state.atmosphere,
       rating: this.state.rating,
       user_id: this.props.user,
       brewery_id: this.props.brewery,
@@ -44,59 +45,43 @@ class NoteContainer extends Component {
   };
 
   render() {
-    const notes = (
-      <svg
-        width="1em"
-        height="1em"
-        viewBox="0 0 16 16"
-        id={this.props.brewery}
-        class="bi bi-pencil"
-        fill="currentColor"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          fill-rule="evenodd"
-          d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5L13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175l-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"
-        />
-      </svg>
-    );
     return (
-      <div className="notes">
-        <div className="note-toggle">
+      <div className='notes'>
+        <div className='note-toggle'>
           <button
             onClick={(e) => this.toggle(e, this.props.brewery)}
-            id="note-button"
+            id='note-button'
           >
-            {notes}
+            {pencil}
           </button>
         </div>
         {this.state.on && (
-          <span id="note-info">
+          <span id='note-info'>
             <form onSubmit={this.handleSubmit}>
-              <div className="star-rating">
+              <div className='star-rating'>
                 <ReactStars
                   number={this.state.rating}
                   count={5}
                   onChange={this.ratingChanged}
                   size={15}
-                  activeColor="#8fa540"
+                  activeColor='#8fa540'
                 />
               </div>
               <br />
               <label>
                 review
                 <input
-                  type="text"
-                  name="review"
+                  type='text'
+                  name='review'
                   value={this.state.review}
                   onChange={this.handleInputChange}
                   required
                 />
               </label>
               <button
-                type="submit"
-                value="Submit"
-                className="note-submit-button"
+                type='submit'
+                value='Submit'
+                className='note-submit-button'
               >
                 Add Note
               </button>
