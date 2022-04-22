e423aed4879b7beda9bc4e6dafdce3db206241e3
Daniel Patnode <dbpatnode@gmail.com>
2022-04-22 14:59:43 -0700

    dom errors gone for favorites

--- src/containers/FavoriteContainer.js
+++ src/containers/FavoriteContainer.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 import Note from './NoteContainer';
-import { phone, map, globe, pencil, trash, star } from '../SVGs/svg';
+import {
+  phone as phoneIcon,
+  map,
+  globe,
+  pencil,
+  trash,
+  star,
+} from '../SVGs/svg';
 
 class FavoriteContainer extends Component {
   removeFavorite = (id) => {
@@ -22,81 +29,93 @@ class FavoriteContainer extends Component {
       <div>
         <h1 className='header'>Favorites</h1>
         <div className='favorite-cards'>
-          {this.props.favorites.map((favorite, index) => (
-            <div className='row'>
-              <div className='card' index={index} key={index}>
-                <a
-                  className='brewery-name'
-                  style={{ display: 'table-cell' }}
-                  href={favorite.brewery.website_url}
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  {favorite.brewery.brewery_name}
-                </a>
+          {this.props.favorites.map((favorite, index) => {
+            const { id } = favorite;
+            const {
+              website_url,
+              brewery_name,
+              brewery_type,
+              street,
+              city,
+              state,
+              postal_code,
+              phone,
+            } = favorite.brewery;
 
-                <h6>Brewery Style: {favorite.brewery.brewery_type}</h6>
-                <p>
-                  <span id='icons'>{map}</span>
+            return (
+              <div key={id} className='row'>
+                <div className='card' index={index} key={index}>
                   <a
-                    href={`https://maps.google.com/?q=${favorite.brewery.street},${favorite.brewery.city},${favorite.brewery.state}`}
-                    className='address'
-                    target='popup'
-                    onClick="window.open('../html-link.htm','name','width=600,height=400')"
+                    className='brewery-name'
+                    style={{ display: 'table-cell' }}
+                    href={website_url}
+                    target='_blank'
+                    rel='noopener noreferrer'
                   >
-                    {favorite.brewery.street} {favorite.brewery.city},{' '}
-                    {favorite.brewery.state} {favorite.brewery.postal_code}
+                    {brewery_name}
                   </a>
-                </p>
-                <a
-                  href={`tel:${favorite.brewery.phone}`}
-                  className='phone-number'
-                >
-                  <span id='icons'>{phone}</span>
 
-                  {''}
-                  {favorite.brewery.phone.length > 0
-                    ? favorite.brewery.phone
-                    : 'No phone number available'}
-                </a>
+                  <h6>Brewery Style: {brewery_type}</h6>
+                  <p>
+                    <span id='icons'>{map}</span>
+                    <a
+                      href={`https://maps.google.com/?q=${street},${city},${state}`}
+                      className='address'
+                      target='popup'
+                    >
+                      {street} {city}, {state} {postal_code}
+                    </a>
+                  </p>
+                  <a href={`tel:${phone}`} className='phone-number'>
+                    <span id='icons'>{phoneIcon}</span>
 
-                <a
-                  className='website'
-                  style={{ display: 'table-cell' }}
-                  href={favorite.brewery.website_url}
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  <span id='icons'>{globe}</span>
-                  {favorite.brewery.website_url}
-                </a>
+                    {''}
+                    {favorite.brewery.phone.length > 0
+                      ? favorite.brewery.phone
+                      : 'No phone number available'}
+                  </a>
 
-                {favorite.brewery.notes.map((note) => (
-                  <div className='favorite-notes'>
-                    <span id='rendered-notes'>previous review:</span>
-                    {note.review}
-                    <br />
-                    {note.rating} <span id='icons'>{star}</span>
-                  </div>
-                ))}
+                  <a
+                    className='website'
+                    style={{ display: 'table-cell' }}
+                    href={favorite.brewery.website_url}
+                    target='_blank'
+                    rel='noopener noreferrer'
+                  >
+                    <span id='icons'>{globe}</span>
+                    {website_url}
+                  </a>
 
-                <Note
-                  pencil={pencil}
-                  addNotes={this.props.addNotes}
-                  brewery={favorite.brewery.id}
-                  user={this.props.user.id}
-                ></Note>
+                  {favorite.brewery.notes.map((note) => {
+                    const { id } = note;
+                    return (
+                      <div key={id} className='favorite-notes'>
+                        <span id='rendered-notes'>previous review:</span>
+                        {note.review}
+                        <br />
+                        {note.rating} <span id='icons'>{star}</span>
+                      </div>
+                    );
+                  })}
 
-                <button
-                  className='submit'
-                  id={favorite.brewery.id}
-                  onClick={() => this.removeFavorite(favorite.brewery.id)}
-                >
-                  {trash}
-                </button>
+                  <Note
+                    pencil={pencil}
+                    addNotes={this.props.addNotes}
+                    brewery={favorite.brewery.id}
+                    user={this.props.user.id}
+                  ></Note>
+
+                  <button
+                    className='submit'
+                    id={favorite.brewery.id}
+                    onClick={() => this.removeFavorite(favorite.brewery.id)}
+                  >
+                    {trash}
+                  </button>
+                </div>
               </div>
-            </div>
-          ))}
+            );
+          })}
         </div>
       </div>
     );
