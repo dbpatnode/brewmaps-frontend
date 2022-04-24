460df618607ba34f0f5d5871f20cf65577431eb3
Daniel Patnode <dbpatnode@gmail.com>
2022-04-24 12:19:58 -0700

    converted NavBar.jsx to functional component

--- src/components/Nav/NavBar.jsx
+++ src/components/Nav/NavBar.jsx
@@ -1,57 +1,37 @@
-import React, { Component } from 'react';
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavLink } from 'react-router-dom';
 
 const logo = './images/hop_logo.png';
 
-class NavBar extends Component {
-  state = {
-    users: [],
-    name: '',
-  };
-
-  handleLogoutClick = () => {
-    this.props.history.push('/');
-    this.props.handleLogout();
-  };
-
-  render() {
-    console.log('navprops ', this.props);
-    return (
-      <div className='navbar'>
-        {this.props.loggedInStatus === 'LOGGED_IN' ? (
-          <nav className='navbar navbar-inverse'>
-            <div className='container-fluid'>
-              <div className='navbar-header'>
-                <img
-                  className='navbar-brand logo'
-                  src={logo}
-                  alt='beer map logo'
-                ></img>
-                BREWMAPS
-              </div>
-              <b>Cheers, {this.props.user.username}!</b>
+const NavBar = ({ loggedInStatus, user, handleLogout }) => {
+  return (
+    <div className='navbar'>
+      <nav className='navbar navbar-inverse'>
+        <div className='container-fluid'>
+          <div className='navbar-header'>
+            <img
+              className='navbar-brand logo'
+              src={logo}
+              alt='beer map logo'
+            ></img>
+            BREWMAPS
+          </div>
+          {loggedInStatus === 'LOGGED_IN' && (
+            <div>
+              <b>Cheers, {user.username}!</b>
               <NavLink to='/map'> BREWERYMAP </NavLink>
               <NavLink to='/breweries'> BREWERIES </NavLink>
               <NavLink to='/favorites'> FAVORITES</NavLink>
-              <a id='logout' href='/' onClick={this.handleLogoutClick}>
+              <a id='logout' href='/' onClick={handleLogout}>
                 LOGOUT
               </a>
             </div>
-          </nav>
-        ) : (
-          <nav className='navbar navbar-inverse'>
-            <div className='container-fluid'>
-              <div className='navbar-header'>
-                <img src={logo} alt='beer map logo' className='logo'></img>
-                BREWMAPS
-              </div>
-            </div>
-          </nav>
-        )}
-      </div>
-    );
-  }
-}
+          )}
+        </div>
+      </nav>
+    </div>
+  );
+};
 
 export default NavBar;
