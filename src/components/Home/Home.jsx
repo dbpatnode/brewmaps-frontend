5f9919f6430a03e44941529be35cdd48b3eb7bbb
Daniel Patnode <dbpatnode@gmail.com>
2022-04-25 14:53:44 -0700

    home page styling web

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -11,33 +11,35 @@ const Home = ({ handleLogin, history }) => {
 
   return (
     <div className='Home'>
-      <span className='home-upper-left'>
-        <div>
-          <h1>BREWMAPS:</h1>
-          <h2>Your</h2>
-          <h2>Brewery</h2>
-          <h2>Wishlist</h2>
-        </div>
-      </span>
-      <span className='home-upper-right'>
-        {!toggle ? (
-          <div className='registration-container'>
-            <Login handleLogin={handleLogin} history={history} />
-            <span className='toggle-message'>
-              Don't have an account yet? signup{' '}
-              <button onClick={handleToggle}>here</button>
-            </span>
+      <div className='home-content'>
+        <span className='home-upper-left'>
+          <div>
+            <h1>BREWMAPS:</h1>
+            <h2>Your</h2>
+            <h2>Brewery</h2>
+            <h2>Wish List</h2>
           </div>
-        ) : (
-          <div className='registration-container'>
-            <Registration handleLogin={handleLogin} history={history} />
-            <span className='toggle-message'>
-              Do you have an account already? Login{' '}
-              <button onClick={handleToggle}>here</button>
-            </span>
-          </div>
-        )}
-      </span>
+        </span>
+        <span className='home-upper-right'>
+          {!toggle ? (
+            <div className='registration-container'>
+              <Login handleLogin={handleLogin} history={history} />
+              <span className='toggle-message'>
+                Don't have an account yet? signup{' '}
+                <button onClick={handleToggle}>here</button>.
+              </span>
+            </div>
+          ) : (
+            <div className='registration-container'>
+              <Registration handleLogin={handleLogin} history={history} />
+              <span className='toggle-message'>
+                Do you have an account already? Login{' '}
+                <button onClick={handleToggle}>here</button>.
+              </span>
+            </div>
+          )}
+        </span>
+      </div>
     </div>
   );
 };
