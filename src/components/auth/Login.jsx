ea64acc9ba9a7151521a28c630ad35f90a822a1b
Daniel Patnode <dbpatnode@gmail.com>
2022-04-28 09:41:55 -0700

    error handling if not proper login

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -8,6 +8,8 @@ const Login = ({ history, handleLogin, loggedInStatus }) => {
     // loginErrors: '',
   });
 
+  const [error, setError] = useState(false);
+
   const handleChange = (e) => {
     let { name, value } = e.target;
 
@@ -20,7 +22,7 @@ const Login = ({ history, handleLogin, loggedInStatus }) => {
   const handleSubmit = (e) => {
     e.preventDefault();
     const { username, password } = inputs;
-    history.push('/map');
+    // history.push('/map');
 
     fetch('https://daniels-brewmaps-api.herokuapp.com/login', {
       method: 'POST',
@@ -34,52 +36,61 @@ const Login = ({ history, handleLogin, loggedInStatus }) => {
     })
       .then((res) => res.json())
       .then((json) => {
-        console.log(json);
         handleLogin(json.user);
         localStorage.setItem('authToken', json.auth_token);
+        history.push('/map');
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+        if (error) {
+          setError(true);
+        }
       });
   };
 
   const { username, password } = inputs;
   return (
     <div>
-      <form className='login' onSubmit={handleSubmit}>
-        <fieldset>
-          <legend className='legend'>Login</legend>
-          <div className='input'>
-            <input
-              type='username'
-              name='username'
-              placeholder='Username'
-              value={username}
-              onChange={handleChange}
-              required
-            />
-            <span>
-              <i className='fa fa-envelope-o'>{userIcon}</i>
-            </span>
-          </div>
-          <div className='input'>
-            <input
-              type='password'
-              name='password'
-              placeholder='Password'
-              value={password}
-              onChange={handleChange}
-              required
-            />
-            <span>
-              <i className='fa fa-lock'>{passwordIcon}</i>
-            </span>
-          </div>
+      <div>
+        <form className='login' onSubmit={handleSubmit}>
+          <fieldset>
+            <legend className='legend'>Login</legend>
+            <div className='input'>
+              <input
+                type='username'
+                name='username'
+                placeholder='Username'
+                value={username}
+                onChange={handleChange}
+                required
+              />
+              <span>
+                <i className='fa fa-envelope-o'>{userIcon}</i>
+              </span>
+            </div>
+            <div className='input'>
+              <input
+                type='password'
+                name='password'
+                placeholder='Password'
+                value={password}
+                onChange={handleChange}
+                required
+              />
+              <span>
+                <i className='fa fa-lock'>{passwordIcon}</i>
+              </span>
+            </div>
 
-          <button className='submit' type='submit'>
-            {arrow}
-          </button>
-          {/* {this.state.isInvalid && <div>Invalid Username or Password</div>} */}
-        </fieldset>
-      </form>
-      <br />
+            <button className='submit' type='submit'>
+              {arrow}
+            </button>
+          </fieldset>
+        </form>
+        <br />
+      </div>
+
+      {error && <div>Invalid Username or Password.</div>}
     </div>
   );
 };
