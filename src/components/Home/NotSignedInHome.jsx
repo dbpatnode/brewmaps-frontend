bfe83aa81dee46e21a787eeb0ca2bbf70e424d6a
Daniel Patnode <dbpatnode@gmail.com>
2022-04-28 11:55:30 -0700

    spacing on home copy and registration div

--- src/components/Home/NotSignedInHome.jsx
+++ src/components/Home/NotSignedInHome.jsx
@@ -11,35 +11,37 @@ const NotSignedInHome = ({ handleLogin, history }) => {
 
   return (
     <div className='home-content'>
-      <span className='home-upper-left'>
-        <div>
-          <h1>BREWMAPS:</h1>
-          <h2>Your</h2>
-          <h2>Brewery</h2>
-          <h2>Wish List</h2>
-        </div>
-      </span>
-      <span className='home-upper-right'>
-        {!toggle ? (
-          <div className='registration-container'>
-            <Login handleLogin={handleLogin} history={history} />
-            <span className='toggle-message'>
-              <p>
-                Don't have an account yet? Sign up{' '}
+      <div className='wrapper'>
+        <span className='home-upper-left'>
+          <div>
+            <h1>BREWMAPS:</h1>
+            <h2>Your</h2>
+            <h2>Brewery</h2>
+            <h2>Wish List</h2>
+          </div>
+        </span>
+        <span className='home-upper-right'>
+          {!toggle ? (
+            <div className='registration-container'>
+              <Login handleLogin={handleLogin} history={history} />
+              <span className='toggle-message'>
+                <p>
+                  Don't have an account yet? Sign up{' '}
+                  <button onClick={handleToggle}>here</button>.
+                </p>
+              </span>
+            </div>
+          ) : (
+            <div className='registration-container'>
+              <Registration handleLogin={handleLogin} history={history} />
+              <span className='toggle-message'>
+                Have an account already? Login{' '}
                 <button onClick={handleToggle}>here</button>.
-              </p>
-            </span>
-          </div>
-        ) : (
-          <div className='registration-container'>
-            <Registration handleLogin={handleLogin} history={history} />
-            <span className='toggle-message'>
-              Have an account already? Login{' '}
-              <button onClick={handleToggle}>here</button>.
-            </span>
-          </div>
-        )}
-      </span>
+              </span>
+            </div>
+          )}
+        </span>
+      </div>
     </div>
   );
 };
